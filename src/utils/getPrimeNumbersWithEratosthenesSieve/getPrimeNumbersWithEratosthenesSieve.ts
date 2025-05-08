7a11c5eacf97fa1bd85882f36fe3fcc57d53e5e7
Andrei Senaev <gsacryfice@gmail.com>
2025-05-08 10:19:31 +0000

    default message

--- src/utils/getPrimeNumbersWithEratosthenesSieve/getPrimeNumbersWithEratosthenesSieve.ts
+++ src/utils/getPrimeNumbersWithEratosthenesSieve/getPrimeNumbersWithEratosthenesSieve.ts
@@ -1,4 +1,6 @@
-export function getPrimeNumbersWithEratosthenesSieve(max) {
+import { PositiveInteger } from '../Number/PositiveInteger';
+
+export function getPrimeNumbersWithEratosthenesSieve(max: PositiveInteger): PositiveInteger[] {
     const sieve = Array.from({ length: max + 1 });
 
     for (let base = 2; base <= max ** 0.5; base++) {
