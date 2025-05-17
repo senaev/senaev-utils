4f03d7a269f153011a5dc73bc59f835bf3761e27
Andrei Senaev <gsacryfice@gmail.com>
2025-05-17 18:12:41 +0000

    default message

--- src/utils/Map/mapGetOrSet/mapGetOrSet.ts
+++ src/utils/Map/mapGetOrSet/mapGetOrSet.ts
@@ -1,4 +1,4 @@
-export const mapGetOrSetIfNotExists = <K, V>(map: Map<K, V>, key: K, value: V): V => {
+export const mapGetOrSet = <K, V>(map: Map<K, V>, key: K, value: V): V => {
     if (!map.has(key)) {
         map.set(key, value);
     }
