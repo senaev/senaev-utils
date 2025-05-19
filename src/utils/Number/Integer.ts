066d921305f6c1058957b54c3d661ad79fbda12f
Andrei Senaev <gsacryfice@gmail.com>
2025-05-19 15:03:25 +0000

    default message

--- src/utils/Number/Integer.ts
+++ src/utils/Number/Integer.ts
@@ -3,11 +3,11 @@
  */
 export type Integer = number;
 
-export function isInteger(value: number): value is Integer {
+export function isInteger(value: unknown): value is Integer {
     return Number.isInteger(value);
 }
 
-export function assertInteger(value: number): asserts value is Integer {
+export function assertInteger(value: unknown): asserts value is Integer {
     if (!isInteger(value)) {
         throw new Error(`value=[${value}] is not an integer`);
     }
