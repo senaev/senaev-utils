cf8c883cb76a0747d29869508bba7f8b226f65ac
Andrei Senaev <gsacryfice@gmail.com>
2025-07-25 12:05:00 +0000

    default message

--- src/utils/Function/tryCatch/tryCatch.ts
+++ src/utils/Function/tryCatch/tryCatch.ts
@@ -1,7 +1,6 @@
 import { AnyFunction } from '../../../types/AnyFunction';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function tryCatch<T extends AnyFunction>(fn: T, onError?: (e: Error) => any): ReturnType<T> | undefined {
+export function tryCatch<T extends AnyFunction>(fn: T, onError?: (e: Error) => void): ReturnType<T> | undefined {
     try {
         return fn();
     } catch (e) {
