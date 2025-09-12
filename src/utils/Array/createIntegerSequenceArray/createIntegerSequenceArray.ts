fa1bf830c115d7676eb13d37a3300148cc5602ea
Andrei Senaev <gsacryfice@gmail.com>
2025-09-12 15:41:26 +0000

    default message

--- src/utils/Array/createIntegerSequenceArray/createIntegerSequenceArray.ts
+++ src/utils/Array/createIntegerSequenceArray/createIntegerSequenceArray.ts
@@ -1,3 +1,3 @@
-import { Integer } from '../../Number/Integer';
+import { Integer } from '../../../types/Number/Integer';
 
 export const createIntegerSequenceArray = (start: Integer, end: Integer): Integer[] => Array.from({ length: end - start + 1 }, (_, i) => start + i);
