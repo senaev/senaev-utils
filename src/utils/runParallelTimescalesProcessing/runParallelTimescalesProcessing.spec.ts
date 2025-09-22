1def1ff12ba05d9e10bd60948b1893446f0c97a3
Andrei Senaev <gsacryfice@gmail.com>
2025-09-22 10:22:36 +0000

    default message

--- src/utils/runParallelTimescalesProcessing/runParallelTimescalesProcessing.spec.ts
+++ src/utils/runParallelTimescalesProcessing/runParallelTimescalesProcessing.spec.ts
@@ -1,7 +1,7 @@
 import {
     describe,
     expect,
-    it, vi,
+    it, Mock, vi,
 } from 'vitest';
 
 import { UnsignedInteger } from '../../types/Number/UnsignedInteger';
@@ -10,10 +10,10 @@ import { waitForFunction } from '../waitForFunction/waitForFunction';
 
 import { runParallelTimescalesProcessing } from './runParallelTimescalesProcessing';
 
-function createTimescale<T extends { time: number }>(callback: (index: UnsignedInteger) => T | undefined): RemoteDataProcessingWindowLoadNextItemsFunction<T> {
+function createTimescale<T extends { time: number }>(callback: (index: UnsignedInteger) => T | undefined): Mock<RemoteDataProcessingWindowLoadNextItemsFunction<T>> {
     let i = 0;
 
-    return ({ count }) => {
+    return vi.fn(({ count }) => {
         const items: T[] = [];
 
         for (let c = 0; c < count; c++) {
@@ -30,7 +30,7 @@ function createTimescale<T extends { time: number }>(callback: (index: UnsignedI
             items,
             isLast: items.length !== count,
         });
-    };
+    });
 }
 
 describe('runParallelTimescalesProcessing', () => {
@@ -87,30 +87,32 @@ describe('runParallelTimescalesProcessing', () => {
     it('should work with multiple different timescales', async () => {
         const callback = vi.fn();
 
+        const extractItemsFunctions = [
+            createTimescale((i) => {
+                if (i > 9) {
+                    return undefined;
+                }
+
+                return {
+                    time: i,
+                    value: String(i),
+                };
+            }),
+            createTimescale((i) => {
+                if (i > 4) {
+                    return undefined;
+                }
+
+                return {
+                    time: i,
+                    value: String(i),
+                };
+            }),
+            createTimescale<{ time: number; value: string }>(() => undefined),
+        ];
+
         runParallelTimescalesProcessing<{ time: number; value: string }[]>({
-            extractItemsFunctions: [
-                createTimescale((i) => {
-                    if (i > 9) {
-                        return undefined;
-                    }
-
-                    return {
-                        time: i,
-                        value: String(i),
-                    };
-                }),
-                createTimescale((i) => {
-                    if (i > 4) {
-                        return undefined;
-                    }
-
-                    return {
-                        time: i,
-                        value: String(i),
-                    };
-                }),
-                createTimescale<{ time: number; value: string }>(() => undefined),
-            ],
+            extractItemsFunctions,
             callback,
             bufferSize: 1000,
         });
@@ -232,6 +234,335 @@ describe('runParallelTimescalesProcessing', () => {
                     undefined,
                     undefined,
                 ],
+            ],
+        ]);
+
+        expect(extractItemsFunctions.map((extractItemsFunction) => extractItemsFunction.mock.calls.length)).toEqual([
+            1,
+            1,
+            1,
+        ]);
+    });
+
+    it('should work with bufferSize', async () => {
+        const callback = vi.fn();
+
+        const extractItemsFunctions = [
+            createTimescale((i) => {
+                if (i > 9) {
+                    return undefined;
+                }
+
+                return {
+                    time: i,
+                    value: String(i),
+                };
+            }),
+            createTimescale((i) => {
+                if (i > 4) {
+                    return undefined;
+                }
+
+                return {
+                    time: i,
+                    value: String(i),
+                };
+            }),
+            createTimescale<{ time: number; value: string }>(() => undefined),
+        ];
+
+        runParallelTimescalesProcessing<{ time: number; value: string }[]>({
+            extractItemsFunctions,
+            callback,
+            bufferSize: 3,
+        });
+
+        await waitForFunction(() => callback.mock.calls.length === 10);
+
+        expect(callback.mock.calls).toEqual([
+            [
+                [
+                    {
+                        time: 0,
+                        value: '0',
+                    },
+                    {
+                        time: 0,
+                        value: '0',
+                    },
+                    undefined,
+                ],
+            ],
+            [
+                [
+                    {
+                        time: 1,
+                        value: '1',
+                    },
+                    {
+                        time: 1,
+                        value: '1',
+                    },
+                    undefined,
+                ],
+            ],
+            [
+                [
+                    {
+                        time: 2,
+                        value: '2',
+                    },
+                    {
+                        time: 2,
+                        value: '2',
+                    },
+                    undefined,
+                ],
+            ],
+            [
+                [
+                    {
+                        time: 3,
+                        value: '3',
+                    },
+                    {
+                        time: 3,
+                        value: '3',
+                    },
+                    undefined,
+                ],
+            ],
+            [
+                [
+                    {
+                        time: 4,
+                        value: '4',
+                    },
+                    {
+                        time: 4,
+                        value: '4',
+                    },
+                    undefined,
+                ],
+            ],
+            [
+                [
+                    {
+                        time: 5,
+                        value: '5',
+                    },
+                    undefined,
+                    undefined,
+                ],
+            ],
+            [
+                [
+                    {
+                        time: 6,
+                        value: '6',
+                    },
+                    undefined,
+                    undefined,
+                ],
+            ],
+            [
+                [
+                    {
+                        time: 7,
+                        value: '7',
+                    },
+                    undefined,
+                    undefined,
+                ],
+            ],
+            [
+                [
+                    {
+                        time: 8,
+                        value: '8',
+                    },
+                    undefined,
+                    undefined,
+                ],
+            ],
+            [
+                [
+                    {
+                        time: 9,
+                        value: '9',
+                    },
+                    undefined,
+                    undefined,
+                ],
+            ],
+        ]);
+
+        expect(extractItemsFunctions.map((extractItemsFunction) => extractItemsFunction.mock.calls)).toEqual([
+            [
+                [
+                    {
+                        count: 3,
+                        lastItem: undefined,
+                    },
+                ],
+                [
+                    {
+                        count: 3,
+                        lastItem: {
+                            time: 2,
+                            value: '2',
+                        },
+                    },
+                ],
+                [
+                    {
+                        count: 3,
+                        lastItem: {
+                            time: 5,
+                            value: '5',
+                        },
+                    },
+                ],
+                [
+                    {
+                        count: 3,
+                        lastItem: {
+                            time: 8,
+                            value: '8',
+                        },
+                    },
+                ],
+            ],
+            [
+                [
+                    {
+                        count: 3,
+                        lastItem: undefined,
+                    },
+                ],
+                [
+                    {
+                        count: 3,
+                        lastItem: {
+                            time: 2,
+                            value: '2',
+                        },
+                    },
+                ],
+            ],
+            [
+                [
+                    {
+                        count: 3,
+                        lastItem: undefined,
+                    },
+                ],
+            ],
+        ]);
+
+        const resultPromisesArrays = extractItemsFunctions.map((extractItemsFunction) => extractItemsFunction.mock.results.map((result) => result.value));
+        const results = await Promise.all(resultPromisesArrays.map((resultPromises) => Promise.all(resultPromises)));
+
+        expect(results).toEqual([
+            [
+                {
+                    isLast: false,
+                    items: [
+                        {
+                            time: 0,
+                            value: '0',
+                        },
+                        {
+                            time: 1,
+                            value: '1',
+                        },
+                        {
+                            time: 2,
+                            value: '2',
+                        },
+                    ],
+                },
+                {
+                    isLast: false,
+                    items: [
+                        {
+                            time: 3,
+                            value: '3',
+                        },
+                        {
+                            time: 4,
+                            value: '4',
+                        },
+                        {
+                            time: 5,
+                            value: '5',
+                        },
+                    ],
+                },
+                {
+                    isLast: false,
+                    items: [
+                        {
+                            time: 6,
+                            value: '6',
+                        },
+                        {
+                            time: 7,
+                            value: '7',
+                        },
+                        {
+                            time: 8,
+                            value: '8',
+                        },
+                    ],
+                },
+                {
+                    isLast: true,
+                    items: [
+                        {
+                            time: 9,
+                            value: '9',
+                        },
+                    ],
+                },
+            ],
+            [
+                {
+                    isLast: false,
+                    items: [
+                        {
+                            time: 0,
+                            value: '0',
+                        },
+                        {
+                            time: 1,
+                            value: '1',
+                        },
+                        {
+                            time: 2,
+                            value: '2',
+                        },
+                    ],
+                },
+                {
+                    isLast: true,
+                    items: [
+                        {
+                            time: 3,
+                            value: '3',
+                        },
+                        {
+                            time: 4,
+                            value: '4',
+                        },
+                    ],
+                },
+            ],
+            [
+                {
+                    isLast: true,
+                    items: [],
+                },
             ],
         ]);
     });
