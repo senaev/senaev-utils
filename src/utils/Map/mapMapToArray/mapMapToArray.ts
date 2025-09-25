886441aa40f83cda2fc337fbb612c8d99fc66914
Andrei Senaev <gsacryfice@gmail.com>
2025-09-25 12:58:46 +0000

    default message

--- src/utils/Map/mapMapToArray/mapMapToArray.ts
+++ src/utils/Map/mapMapToArray/mapMapToArray.ts
@@ -1,6 +1,4 @@
-import { MapAsState } from '../../../reactHooks/useMapAsState/useMapAsState';
-
-export function mapMapToArray<K, V, R>(map: MapAsState<K, V>, callback: (value: V, key: K) => R): R[] {
+export function mapMapToArray<K, V, R>(map: Map<K, V>, callback: (value: V, key: K) => R): R[] {
     return Array.from(map.entries(), ([
         key,
         value,
