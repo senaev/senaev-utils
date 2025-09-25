0dfdfd5de10f2b351f320c985afe2f53805bf8cb
Andrei Senaev <gsacryfice@gmail.com>
2025-09-25 15:40:58 +0000

    default message

--- src/utils/Ranges/getDistanceToRange/getDistanceToRange.ts
+++ src/utils/Ranges/getDistanceToRange/getDistanceToRange.ts
@@ -1,6 +1,8 @@
+import { FiniteNumber } from '../../../types/Number/FiniteNumber';
+import { UnsignedFiniteNumber } from '../../../types/Number/UnsignedFiniteNumber';
 import { RangeTuple } from '../../../types/RangeTuple';
 
-export function getDistanceToRange(range: RangeTuple<number>, value: number): number {
+export function getDistanceToRange(range: RangeTuple<FiniteNumber>, value: FiniteNumber): UnsignedFiniteNumber {
     if (value >= range[0] && value <= range[1]) {
         return 0;
     }
