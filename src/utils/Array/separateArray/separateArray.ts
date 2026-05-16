9453d790c6dcf81d6cbd0658860cb2851ccaaf91
Andrei Senaev <andrei.senaev@gmail.com>
2026-05-16 13:14:07 +0200

    default message

--- src/utils/Array/separateArray/separateArray.ts
+++ src/utils/Array/separateArray/separateArray.ts
@@ -2,23 +2,23 @@ export function separateArray<T>(
     array: Iterable<T>,
     separateFunction: (value: T, index: number, currentArray: Iterable<T>) => boolean
 ): [T[], T[]] {
-    const appropriate: T[] = [];
-    const inappropriate: T[] = [];
+    const suitable: T[] = [];
+    const unsuitable: T[] = [];
 
     let index = 0;
 
     for (const value of array) {
         if (separateFunction(value, index, array)) {
-            appropriate.push(value as T);
+            suitable.push(value as T);
         } else {
-            inappropriate.push(value);
+            unsuitable.push(value);
         }
 
         index++;
     }
 
     return [
-        appropriate,
-        inappropriate,
+        suitable,
+        unsuitable,
     ];
 }
